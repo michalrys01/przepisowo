bedd07176b75ecff79eed4ca561baa57868f8766
Pawel <pyrgiel77@wp.pl>
2024-01-25 18:30:22 +0100

    25.01.2023 Dodano obsługe errora podczas wpisywania błędnej lokalizacji do API pogodowego. Dodano metodę try catch w klasie WeatherService.java, aby wyświetlała w konsoli informacje o tym, że użytkownik podał złą lokalizację.
    Dodano również w WeatherController.java informację dla użytkownika, że podał złą lokalizację, a strona przekierowuje go do strony /weather

--- src/main/java/przepisowoaplikacja/przepisowoaplikacja/controllers/WeatherController.java
+++ src/main/java/przepisowoaplikacja/przepisowoaplikacja/controllers/WeatherController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import przepisowoaplikacja.przepisowoaplikacja.models.WeatherData;
 import przepisowoaplikacja.przepisowoaplikacja.services.WeatherService;
 
@@ -15,15 +16,18 @@ public class WeatherController {
     private WeatherService weatherService;
 
     @GetMapping("/weather")
-    public String getWeather(@RequestParam(defaultValue = "Lublin") String city, Model model) {
+    public String getWeather(@RequestParam(defaultValue = "Lublin") String city, Model model, RedirectAttributes redirectAttributes) {
         WeatherData weatherData = weatherService.getWeatherData(city);
 
         if (weatherData != null) {
             model.addAttribute("city", city);
             model.addAttribute("temperature", weatherData.getMain().get("temp"));
             model.addAttribute("wind", weatherData.getWind().get("speed"));
+            return "weather";
+        } else {
+            // Dodaj atrybut do modelu z informacją o błędzie
+            model.addAttribute("errorMessage", "Nie znaleziono danych pogodowych dla miasta: " + city);
+            return "weather";
         }
-
-        return "weather";
     }
 }
--- src/main/java/przepisowoaplikacja/przepisowoaplikacja/models/WeatherData.java
+++ src/main/java/przepisowoaplikacja/przepisowoaplikacja/models/WeatherData.java
@@ -1,34 +1,24 @@
 package przepisowoaplikacja.przepisowoaplikacja.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherData {
     private Map<String, Object> main;
     private List<Map<String, Object>> weather;
     private Map<String, Object> wind;
 
-    public Map<String, Object> getMain() {
-        return main;
-    }
-
     public void setMain(Map<String, Object> main) {
         this.main = main;
     }
 
-    public List<Map<String, Object>> getWeather() {
-        return weather;
-    }
-
     public void setWeather(List<Map<String, Object>> weather) {
         this.weather = weather;
-    }
-
-    public Map<String, Object> getWind() {
-        return wind;
     }
 
     public void setWind(Map<String, Object> wind) {
